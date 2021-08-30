c8c18b1b99277605faf4f96e168483f10a223379
Christian Rimondi <christian.rimondi@gmail.com>
2021-08-30 11:32:59 +0200

    Create factories
    Create the afterpay_payment_method, afterpay_payment, and
    afterpay_payment_source factories.

--- lib/solidus_afterpay/testing_support/factories.rb
+++ lib/solidus_afterpay/testing_support/factories.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :afterpay_payment_method, class: SolidusAfterpay::PaymentMethod do
+    name { 'Afterpay' }
+  end
+end
+
+FactoryBot.define do
+  factory :afterpay_payment, parent: :payment do
+    association(:payment_method, factory: :afterpay_payment_method)
+  end
+end
+
+FactoryBot.define do
+  factory :afterpay_payment_source, class: SolidusAfterpay::PaymentSource do
+    association(:payment_method, factory: :afterpay_payment_method)
+    token { "12345678910" }
+  end
 end
