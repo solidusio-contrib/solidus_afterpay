a3ecd1031722b4460dc0be9cb304ffab5e5b4dd0
Christian Rimondi <christian.rimondi@gmail.com>
2021-09-08 10:41:19 +0200

    Use the new Afterpay global-api endpoint
    Afterpay recently released a new unified endpoint for AU, NZ, US, CA, UK, FR,
    IT and ES.
    
    https://afterpay-global-api.readme.io/reference/api-environments

--- app/models/solidus_afterpay/gateway.rb
+++ app/models/solidus_afterpay/gateway.rb
@@ -10,7 +10,7 @@ module SolidusAfterpay
       ::Afterpay.configure do |config|
         config.merchant_id = options[:merchant_id]
         config.secret_key = options[:secret_key]
-        config.server = 'https://api.us-sandbox.afterpay.com/' if options[:test_mode]
+        config.server = 'https://global-api-sandbox.afterpay.com/' if options[:test_mode]
         config.user_agent = SolidusAfterpay::UserAgentGenerator.new(merchant_id: options[:merchant_id]).generate
       end
     end
